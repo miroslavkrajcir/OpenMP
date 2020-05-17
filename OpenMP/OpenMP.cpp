b79b158b6ed4f90de50076e01ece96bcb40f32bc
miroslavkrajcir <miroslav_krajcir@yahoo.com>
2020-05-17 15:42:32 +0300

    final

--- OpenMP/OpenMP.cpp
+++ OpenMP/OpenMP.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <thread>
 #include <vector>
-#include <omp.h>
 #include <stdio.h>
+
+#include <omp.h>
 
 using namespace std;
 
@@ -32,46 +33,26 @@ public:
 	}
 };
 
-vector<Instrument*> instruments;
-
-void do_work(int me, int total) {
-	// printf("%d %d\n", me, total);
-
-	int first = 1024 * me / total;
-	int last = 1024 * (me + 1) / total - 1;
-
-	for (int i = first; i <= last; i++) {
-		// printf("i = %d\n", i);
-		instruments[i]->generateData();
-	}
-}
-
 int main() {
+	vector<Instrument*> instruments;
+
+	// generate instruments
 	for (unsigned int i = 0; i < 1024; ++i) {
 		instruments.push_back(new Instrument(i));
 	}
 
+	// do magic
 	omp_set_num_threads(omp_get_num_procs());
-	int me;
-	int total = omp_get_num_threads();
+#pragma omp parallel default(none)
+	{
+		printf("omp_get_thread_num() = %d\n", omp_get_thread_num());
+	}
 
-#pragma omp parallel default(none), private(me), shared(total) {
-	total = omp_get_num_threads();
-	me = omp_get_thread_num();
-	do_work(me, total); }
-
-	cout << "koniec" << endl;
+	// do magic
+	omp_set_num_threads(omp_get_num_procs());
+#pragma omp parallel for
+	for (int i = 0; i < 1024; ++i) {
+		printf("omp_get_thread_num() = %d\n", omp_get_thread_num());
+		instruments[i]->generateData();
+	}
 }
-
-
-/*
-#ifndef _OPENMP
-	// cout << "OpenMP IS NOT supported – sorry!" << endl;
-	// exit(0);
-#endif
-
-#ifdef _OPENMP
-	// cout << "OpenMP IS supported" << endl;
-	// exit(0);
-#endif
-*/
